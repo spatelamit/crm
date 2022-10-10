b87b8562dcdbb613364502279ecaa13da74d095f
pankaj manjhic04 <pankajmanjhic04@gmail.com>
2022-10-10 18:00:09 +0530

    pankaj

---
+++ app/Http/Controllers/usercontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class usercontroller extends Controller
+{
+    public function User_dashboard(){
+
+        dd("pankaj");
+        return view('user_dashboard');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\usercontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+   Route::get('/User_dashboard',[usercontroller::class, 'User_dashboard']);
+
