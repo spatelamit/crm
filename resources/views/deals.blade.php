3660a93ef4502b3e8f73233c89403e26560146b2
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-11 11:17:17 +0530

    ashish

---
+++ resources/views/deals.blade.php
@@ -0,0 +1,47 @@
+@include('header')
+<div class="breadcrumbbar">
+                <div class="row align-items-center">
+                    <div class="col-md-8 col-lg-8">
+                        <h4 class="page-title">CRM</h4>
+                        <div class="breadcrumb-list">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Deals</li>
+                            </ol>
+                        </div>
+                    </div>
+                    
+                </div>          
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ @include('footer')
