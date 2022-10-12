4c469cb7b815c9081ab9d405d3fb581ff69f9b30
pankaj manjhic04 <pankajmanjhic04@gmail.com>
2022-10-12 14:05:02 +0530

    pankaj2

--- app/Http/Controllers/UserSettingcontroller.php
+++ app/Http/Controllers/UserSettingcontroller.php
@@ -12,14 +12,7 @@ use App\Models\Usersetting;
 class UserSettingController extends Controller
 
 {
-<<<<<<< HEAD
 
-=======
-	public function __construct() {
-        $Usersetting=new Usersetting();
-    }
-    
->>>>>>> 1109bafd8cecde85c9ca0785d851cc9b7fa466c4
     		public function roles_view ()
 
     		{
@@ -32,17 +25,17 @@ class UserSettingController extends Controller
     		}
     		public function add_role ()
     		{
-<<<<<<< HEAD
+
 
     			return view('roles_setting/add-role');
 
-=======
+
     			$Usersetting=new Usersetting();
     			$data['allfeatures']=$Usersetting->GetFeatures();
-    			
+
     			return view('roles_setting/add-role',compact('data'));
-    		
->>>>>>> 1109bafd8cecde85c9ca0785d851cc9b7fa466c4
+
+
     		}
 
      public function rolestree(){
