e6e7bcc130ca48bf3cad4f9c739f792e6e6f356c
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-13 14:47:34 +0530

    ashish

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -1,1 +1,40 @@
-asds
+
+@include('header')
+
+<div class="content-wrapper">
+
+<div class="row"> 
+
+<div class="col-md-12">
+<div class="star_barhome">
+
+<ul>
+<li>
+
+
+</li>
+
+
+</ul>
+</div>
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+@include('footer')
