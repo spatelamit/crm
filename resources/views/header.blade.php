3f04a2a4aee36a4cac07ab96a2a50710f0c78c16
Amit Patel <spatelamit@gmail.com>
2022-10-13 16:14:04 +0530

    Update header.blade.php
    amit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,28 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
-    <title>Regal Admin</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- base:css -->
-    <link rel="stylesheet" href="{{ url('/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
-    <link rel="stylesheet" href="{{ url('/assets/vendors/feather/feather.css') }}">
-    <link rel="stylesheet" href="{{ url('/assets/vendors/base/vendor.bundle.base.css') }}">
-    <!-- endinject -->
-    <!-- plugin css for this page -->
-    <link rel="stylesheet" href="{{ url('/assets/vendors/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ url('/assets/vendors/jquery-bar-rating/fontawesome-stars-o.css') }}">
-    <link rel="stylesheet" href="{{ url('/assets/vendors/jquery-bar-rating/fontawesome-stars.css') }}">
-    <!-- End plugin css for this page -->
-    <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
-    <!-- inject:css -->
-    <link rel="stylesheet" href="{{ url('/assets/css/style.css') }}">
-    <!-- endinject -->
-    <link rel="shortcut icon" href="{{ url('/assets/images/favicon.png') }}" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/css/toastr.css" rel="stylesheet" />
-=======
   <title> {{ Config::get('app.title') }} </title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
@@ -44,18 +22,8 @@
   <!-- endinject -->
   <link rel="shortcut icon" href="{{url('/assets/images/favicon.png')}}" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/css/toastr.css" rel="stylesheet" />
->>>>>>> c7fec02d8e7c45498ff315ac7cde0d1db95bb615
 </head>
-
 <body>
-<<<<<<< HEAD
-    <div class="container-scroller">
-        <!-- partial:partials/_navbar.html -->
-        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo" href="{{ url('') }}">Bulk24CRM</a>
-                <a class="navbar-brand brand-logo-mini" href="{{ url('') }}">B</a>
-=======
 <div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
@@ -76,35 +44,18 @@
                 </span>
               </div>
               <input type="text" class="form-control" placeholder="Search Projects.." aria-label="search" aria-describedby="search">
->>>>>>> c7fec02d8e7c45498ff315ac7cde0d1db95bb615
             </div>
-            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-                    <span class="icon-menu"></span>
-                </button>
-                <ul class="navbar-nav mr-lg-2">
-                    <li class="nav-item nav-search d-none d-lg-block">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="search">
-                                    <i class="icon-search"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="Search Projects.."
-                                aria-label="search" aria-describedby="search">
-                        </div>
-                    </li>
-                </ul>
-                <ul class="navbar-nav navbar-nav-right ">
-                    <li class="nav-item dropdown d-lg-flex d-none">
-                        <button type="button" class="btn btn-info font-weight-bold">+ Create New</button>
-                    </li>
-                    <li class="nav-item dropdown d-flex">
-                        <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center"
-                            id="messageDropdown" href="#" data-toggle="dropdown">
-                            <i class="icon-air-play mx-0"></i>
-                        </a>
-                        <!-- <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-nav-right ">
+            <li class="nav-item dropdown d-lg-flex d-none">
+                <button type="button" class="btn btn-info font-weight-bold">+ Create New</button>
+            </li>
+          <li class="nav-item dropdown d-flex">
+            <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-toggle="dropdown">
+              <i class="icon-air-play mx-0"></i>
+            </a>
+            <!-- <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
               <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
               <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
@@ -143,45 +94,41 @@
                 </div>
               </a>
             </div> -->
-                    </li>
-                    <li class="nav-item dropdown d-flex mr-4 ">
-                        <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center"
-                            id="notificationDropdown" href="#" data-toggle="dropdown">
-                            <i class="icon-cog"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
-                            aria-labelledby="notificationDropdown">
-                            <p class="mb-0 font-weight-normal float-left dropdown-header">Settings</p>
-                            <a class="dropdown-item preview-item">
-                                <i class="icon-head"></i> Profile
-                            </a>
+          </li>
+          <li class="nav-item dropdown d-flex mr-4 ">
+            <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="#" data-toggle="dropdown">
+              <i class="icon-cog"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+              <p class="mb-0 font-weight-normal float-left dropdown-header">Settings</p>
+              <a class="dropdown-item preview-item">
+                  <i class="icon-head"></i> Profile
+              </a>
 
-                            <a class="dropdown-item preview-item" href="{{ url('logout') }}">
+              <a class="dropdown-item preview-item" href="{{url('logout')}}">
 
-                                <a class="dropdown-item preview-item" href="{{ url('/logout') }}">
+              <a class="dropdown-item preview-item" href="{{url('/logout')}}">
 
-                                    <i class="icon-inbox"></i> Logout
-                                </a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown mr-4 d-lg-flex d-none">
-                        <a class="nav-link count-indicatord-flex align-item s-center justify-content-center"
-                            href="#">
-                            <i class="icon-grid"></i>
-                        </a>
-                    </li>
-                </ul>
-                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
-                    data-toggle="offcanvas">
-                    <span class="icon-menu"></span>
-                </button>
+                  <i class="icon-inbox"></i> Logout
+              </a>
             </div>
-        </nav>
-        <!-- partial -->
-        <div class="container-fluid page-body-wrapper">
-            <!-- partial:partials/_sidebar.html -->
-            <nav class="sidebar sidebar-offcanvas" id="sidebar">
-                <!--<div class="user-profile">
+          </li>
+          <li class="nav-item dropdown mr-4 d-lg-flex d-none">
+            <a class="nav-link count-indicatord-flex align-item s-center justify-content-center" href="#">
+              <i class="icon-grid"></i>
+            </a>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <!--<div class="user-profile">
           <div class="user-image">
             <img src="assets_2/images/faces/face28.png">
           </div>
@@ -192,82 +139,13 @@
               Developer
           </div>
         </div>-->
-<<<<<<< HEAD
-                <ul class="nav main-menu menu-fixed">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('user_dashboard') }}">
-                            <i class="icon-pie-graph  menu-icon"></i>
-                            <span class="menu-title">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                            aria-controls="ui-basic">
-                            <i class="icon-cog menu-icon"></i>
-                            <span class="menu-title"> CRM Setting </span>
-                            <i class="menu-arrow"></i>
-                        </a>
-                        <div class="collapse" id="ui-basic">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"><a class="nav-link" href="{{ url('roles') }}">Roles
-                                        Setting</a></li>
-                                <li class="nav-item"><a class="nav-link"
-                                        href="{{ url('user_account_setting') }}">Users Account Setting</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('sms_settings') }}">SMS
-                                        Setting</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('email_settings') }}">Email
-                                        Setting</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('forms') }}">Forms</a></li>
-                                <li class="nav-item"><a class="nav-link"
-                                        href="{{ url('pipelines_stages') }}">Pipelines & Stages</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('') }}">Data Admin</a>
-                                </li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('theme_settings') }}">Theme
-                                        Settings</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ url('modules') }}">Modules </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('deals') }}">
-                            <i class="icon-file menu-icon"></i>
-                            <span class="menu-title"> Deals </span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa fa-filter menu-icon"></i>
-                            <span class="menu-title"> Universal Filter</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <!-- partial -->
-            <div class="main-panel">
-=======
         <ul class="nav main-menu menu-fixed">
-        
-         <li class="nav-item">
-            <a class="nav-link" href="{{url('home')}}">
-              <i class="fa fa-home  menu-icon"></i>
-              <span class="menu-title">Home</span>
-            </a>
-          </li>
-        
-        
           <li class="nav-item">
             <a class="nav-link" href="{{url('user_dashboard')}}">
               <i class="icon-pie-graph  menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
           </li>
-          
-          
-         
-          
-          
-          
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="icon-cog menu-icon"></i>
@@ -304,4 +182,3 @@
       </nav>
       <!-- partial -->
       <div class="main-panel">
->>>>>>> c7fec02d8e7c45498ff315ac7cde0d1db95bb615
