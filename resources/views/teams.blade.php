7e11af8a8a90a79367a5189814c46aabe53da5e5
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-15 14:15:04 +0530

    ashish

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -1,1 +1,101 @@
-teams
+@include('header')
+
+<div class="content-wrapper">
+
+<div class="row">
+            <div class="col-sm-12 mb-4 mb-xl-0">
+              <h4 class="font-weight-bold text-dark"> My Team </h4>
+              <p class="font-weight-normal mb-2 text-muted"> Company Name : <strong>ShreeRam Technology PVT LTD</strong> </p>
+            </div>
+          </div>
+          
+          
+          
+   <div class="row">
+   
+   <div class="col-sm-8">
+   <ul>
+   
+   <li>
+   <div class="row">
+   
+   <div class="col-md-4">
+   <div class="font-weight-bold mr-sm-4">
+                              <div>Deposit has updated to Paid</div>
+                              <div class="text-muted font-weight-normal mt-1">32 Minutes Ago</div>
+                            </div>
+   </div>
+   
+   <div class="col-md-4">
+   a
+   </div>
+
+
+<div class="col-md-4">
+   a
+   </div>
+
+   
+   </div>
+   </li>
+   
+   </ul>
+   </div>
+   
+   
+   <div class="col-sm-4">
+   
+   </div>
+   
+   
+   
+   </div>       
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+</div>          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ @include('footer')
