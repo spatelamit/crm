0f156ab7659d3497724719219e0643fdd703d7a6
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-15 15:17:29 +0530

    ashish

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -14,37 +14,334 @@
    <div class="row">
    
    <div class="col-sm-8">
-   <ul>
-   
-   <li>
-   <div class="row">
-   
-   <div class="col-md-4">
-   <div class="font-weight-bold mr-sm-4">
-                              <div>Deposit has updated to Paid</div>
-                              <div class="text-muted font-weight-normal mt-1">32 Minutes Ago</div>
+    <div class="card">
+     <div class="card-body">
+   <div class="table-responsive mt-1">
+                      <table class="table table-header-bg">
+                        <thead>
+                          <tr>
+                            <th>
+                               My Team 
+                            </th>
+                            <th style="text-align:center;">
+                                Leads 
+                            </th>
+                            <th style="text-align:center;">
+                                Last interaction
+                            </th>
+                            
+                            <th style="text-align:center;">
+                            Working Performance
+                            </th>
+                            
+                          </tr>
+                        </thead>
+                        <tbody>
+                        
+                        
+                          <tr>
+                            <td>
+                            <div class="row">
+                            
+                            <div class="col-md-2">
+                            <img src="assets/img/demoimg.png" />
                             </div>
-   </div>
-   
-   <div class="col-md-4">
-   a
-   </div>
+                            
+                            <div class="col-md-10">
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> Ashish Panwar </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                            </div>
+                            
+                            </div>
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-male" aria-hidden="true"> </i><br />
+                              10 Leads
 
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i><br />
+                              10-10-2022
+		
+                            </td>
+                            
+                            <td align="center">
+                            Best
+                            </td>
+                            
+                            
+                          </tr>
+                          
+                          <tr>
+                            <td>
+                            <div class="row">
+                            
+                            <div class="col-md-2">
+                            <img src="assets/img/demoimg.png" />
+                            </div>
+                            
+                            <div class="col-md-10">
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> Amit </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                            </div>
+                            
+                            </div>
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-male" aria-hidden="true"> </i><br />
+                              10 Leads
 
-<div class="col-md-4">
-   a
-   </div>
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i><br />
+                              10-10-2022
+		
+                            </td>
+                            
+                             <td align="center">
+                             Best
+                            </td>
+                          </tr>
+                          
+                          
+                          <tr>
+                            <td>
+                            <div class="row">
+                            
+                            <div class="col-md-2">
+                            <img src="assets/img/demoimg.png" />
+                            </div>
+                            
+                            <div class="col-md-10">
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> AnitRaj </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                            </div>
+                            
+                            </div>
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-male" aria-hidden="true"> </i><br />
+                              10 Leads
 
-   
-   </div>
-   </li>
-   
-   </ul>
-   </div>
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i><br />
+                              10-10-2022
+		
+                            </td>
+                            
+                             <td align="center">
+                             Best
+                            </td>
+                          </tr>
+                          
+                          
+                          
+                          
+                          
+                        </tbody>
+                      </table>
+                    </div>
+                    </div>
+                    </div>
+                    
+                    
+                    
+                   <div class="card mt-3"> 
+                   
+                   <div class="card-header">
+                   Invite Coworker 
+                   </div>
+                   
+                 <div class="card-body ">
+   <div class="table-responsive mt-1">
+                      <table class="table table-header-bg">
+                        <thead>
+                          <tr>
+                            <th>
+                             First Name
+                            </th>
+                            <th style="text-align:center;">
+                               Invited On 
+                            </th>
+                            <th style="text-align:center;">
+                               Pending Sign In
+                            </th>
+                            
+                            <th style="text-align:center;">
+                            Resend Invite
+                            </th>
+                            
+                          </tr>
+                        </thead>
+                        <tbody>
+                        
+                        
+                          <tr>
+                            <td>
+
+                            
+                   
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> Ashish Panwar </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                        
+                            
+                     
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true" style="color:#090;"></i> Invited On <strong>10-10-2022</strong>
+
+                            </td>
+                            <td align="center">
+                             
+                           <i class="fa fa-flag" aria-hidden="true" style="color:red;"></i>
+  Pending Sign Up
+		
+                            </td>
+                            
+                            <td align="center">
+                           
+                           <button type="button" class="btn btn-sm btn btn-info">  Resend Invite </button>
+                            </td>
+                            
+                            
+                          </tr>
+                          
+                          <tr>
+                            <td>
+
+                            
+                   
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> Ashish Panwar </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                        
+                            
+                     
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true" style="color:#090;"></i> Invited On <strong>10-10-2022</strong>
+
+                            </td>
+                            <td align="center">
+                             
+                           <i class="fa fa-flag" aria-hidden="true" style="color:red;"></i>
+  Pending Sign Up
+		
+                            </td>
+                            
+                            <td align="center">
+                           
+                           <button type="button" class="btn btn-sm btn btn-info">  Resend Invite </button>
+                            </td>
+                            
+                            
+                          </tr>
+                          
+                          
+                          <tr>
+                            <td>
+
+                            
+                   
+                            <div class="font-weight-bold mr-sm-4">
+                              <div> Ashish Panwar </div>
+                              <div class="text-muted font-10 font-weight-normal mt-1"> Last Login : <strong> 15-10-2022 </strong>  </div>
+                            </div>
+                        
+                            
+                     
+                            
+                              
+                            </td>
+                            <td align="center">
+                              <i class="fa fa-clock-o" aria-hidden="true" style="color:#090;"></i> Invited On <strong>10-10-2022</strong>
+
+                            </td>
+                            <td align="center">
+                             
+                           <i class="fa fa-flag" aria-hidden="true" style="color:red;"></i>
+  Pending Sign Up
+		
+                            </td>
+                            
+                            <td align="center">
+                           
+                           <button type="button" class="btn btn-sm btn btn-info">  Resend Invite </button>
+                            </td>
+                            
+                            
+                          </tr>
+                          
+                          
+                          
+                          
+                          
+                        </tbody>
+                      </table>
+                    </div>
+                    </div>   
+                    
+                    </div>
+                    
+                    
+                    
+                    </div>
+ 
    
    
    <div class="col-sm-4">
+   <div class="card">
    
+   <div class="card-header">
+   Invite A Coworker
+   </div>
+   <div class="card-body">
+     
+    <div class="form-group">
+     <input type="text" class="form-control" placeholder="first name">
+     </div> 
+     
+     <div class="form-group">
+     <input type="text" class="form-control" placeholder="Mobile Number">
+     </div>
+     
+     <div class="form-group">
+     <input type="text" class="form-control" placeholder="E-mail">
+     </div>
+     
+     <div class="form-group">
+     <input type="text" class="form-control" placeholder="Send Personal Message">
+     </div>
+     
+     <div class="form-group">
+     <button type="button" class="btn btn-info font-weight-bold"> Send Invite </button>
+     </div>
+     
+                 
+  </div>
+  
+  
+              </div>
    </div>
    
    
