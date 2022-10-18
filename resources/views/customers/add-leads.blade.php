60c75fc9463765ba7ec0e83d6e0d1970324b242b
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-18 11:18:03 +0530

    ashish

--- resources/views/customers/add-leads.blade.php
+++ resources/views/customers/add-leads.blade.php
@@ -1,57 +1,66 @@
 @include('header')
 <div class="content-wrapper">
 
-	  <div class="row">
+
+        
+        <div class="row">
         <div class="col-sm-8 mb-4 mb-xl-0">
-            <h4 class="font-weight-bold text-dark"> Leads </h4>
+            <h4 class="font-weight-bold text-dark"> Add Leads </h4>
+            <p class="font-weight-normal mb-2 text-muted">Note : .......</p>
            
         </div>
-       
+        <div class="col-sm-4 mb-4 mb-xl-0">
+            <div class="widgetbar text-right">
+                <a class="btn btn-info font-weight-bold mb-2" href="{{url('module-layout',$data['selected_fields'][0]->module_id)}}">Create Dynamic Layout </a>
+            </div>
+        </div>
     </div>
-    <a href="{{url('module-layout',$data['selected_fields'][0]->module_id)}}">Layout</a>
-  <div class=" right" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog  slideInRight  animated" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle-1"> template</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
-      </div>
+        
+        
+      <div class="row">
+      <div class="col-md-12">
+      <div class="card">
+      <div class="card-body">
+      
       <form method="post" action="{{url('save-leads')}}" autocomplete="nope">
               @csrf
-      <div class="modal-body">
+     
       	<input type="hidden" name="module_id" value="{{$data['selected_fields'][0]->module_id}}">
+        <div class="row">
       	@foreach($data['selected_fields'] as $val)
+        
+        
+        
+        
+        <div class="col-md-3">
         <div class="form-group mb-10">
           <label>{{ str_replace("_"," ",strtoupper($val->col_name)) }}</label>
           <input type="hidden" name="column_id[]" value="{{$val->column_id}}">
           <input type="{{$val->type}}"  name="value[]" class="form-control bg_input" placeholder="Enter your {{ str_replace('_','' ,strtoupper($val->col_name)) }}" >
         </div>
+        </div>
+        
+       
+        
         @endforeach
-       <!--  <div class="form-group mb-10">
-          <label>Sender Id:</label>
-          <input type="text" placeholder="Enter your Sender Id (Its should be 6  characters)" pattern="[A-Za-z]{6}" title="6 haracters allowd, not a numerics " name="sender_id" class="form-control bg_input " required>
-        </div>
-        <div class="form-group mb-10">
-          <label>PE ID:</label>
-          <input type="text" pattern="[0-9]{19}" title="PE ID  should be 19 digit" name="pe_id" class="form-control bg_input" placeholder="Enter your PE ID (Its should be 19 digit)" required>
-        </div>
-        <div class="form-group mb-10">
-          <label>Template_ID:</label>
-          <input type="text" name="template_id" pattern="[0-9]{19}" title="template ID  should be 19 digit" class="form-control bg_input" placeholder="Enter Template ID " required>
-        </div>
-        <div class="form-group mb-10">
-          <label>Content:</label>
-          <textarea name="template" class="form-control bg_input" placeholder="Enter your Content" required></textarea>
-        </div>
-      </div> -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save</button>
-      </div>
-    </form>
+         </div>
+      
+<div class="col-md-12 text-right">
+        <button type="submit" class="btn btn-info font-weight-bold mb-2 ">Save</button>
+    </div>    
 
-    </div>
-  </div>
-</div>
+    </form>
+      
+      
+      </div>
+      </div>
+      </div>
+      </div> 
+
+    
+    
+    
+   
+  
 </div>
 @include('footer')
