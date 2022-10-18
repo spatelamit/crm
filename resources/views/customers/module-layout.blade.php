8f9398f70ade54af60897006e80e91e98d9ea7a8
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-18 10:51:00 +0530

    ashish

--- resources/views/customers/module-layout.blade.php
+++ resources/views/customers/module-layout.blade.php
@@ -1,13 +1,34 @@
-
-@include('header')
-
-
+ @include('header')
 <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
          rel = "stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
-      <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script> 
+<script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<style>
 
+.padd_50 {
+  padding: 30px 10px;
+  background-color: #fff;
+  border: 1px #000 dotted;
+}
 
+.form_D_hedding {
+  padding: 10px 10px;
+  margin: 10px 0px;
+  border-bottom:1px solid #ccc;
+  
+}
+
+.form_D_hedding h6{
+  margin:0px;
+}
+.Selected_green i {
+  color: green;
+}
+.Selected_red i {
+  color: red;
+}
+
+</style>
 <?php
 foreach ($data['selected_fields'] as $key => $selected_field) {
 $fieldl[]=$selected_field->column_id;
@@ -17,139 +38,77 @@ $fieldl[]=$selected_field->column_id;
  $selfield1=implode(",",  $selfield);
  // print_r($selfield1);
 ?>
- 
 <div class="content-wrapper">
+  <div class="row">
+    <div class="col-sm-12 mb-4 mb-xl-0">
+      <h4 class="font-weight-bold text-dark"> {{$data['selected_fields'][0]->modules_name}}</h4>
+      <p class="font-weight-normal mb-2 text-muted">Note : Ashish panwar</p>
+    </div>
+  </div>
+  <div class="row">
+  
+  
+    <div class="col-md-12">
+     <div class="form_D_hedding"> <h3> Create Dynamic Layout </h3> </div>
  
-   <div class="row">
-  
-              <div class="col-md-3">
-              
-                <ul  class="nav flex-column nav-tabs text-center" id="ex1" role="tablist" >
-                  
-            	
-            	
-                
-                <li class="nav-item active "  >  <a class="nav-link  " data-toggle="tab" role="tab" href="#" >{{$data['selected_fields'][0]->modules_name}} </a> </li>
-             
-               
-                </ul>
-              </div>
-  
-
-  <div class="col-md-9">
-  
-
-  
-    <div class="tab-content" >
+    </div>
     
-	
-      <div
-        class="tab-pane  active in "
-        id="list{{$data['mid']}}"
-        role="tabpanel">
-       
-            <div class="row">
-              <div class="layoutfiled">
-                    <ul  class="nav  "  role="tablist" >
-                       <li class="nav-item active"  >  <a class="nav-link  " data-toggle="tab" role="tab" href="#layout"   >Layout</a> </li>
-                         
-                     </ul>
-              </div>
-              
+    
+    <div class="col-md-4">
+      <div class="Selected_green">
+        <h4><i class="fa fa-check" aria-hidden="true"></i> Selected</h4>
+         <p class="font-weight-normal mb-2 text-muted">Note : Ashish panwar</p>
+         <hr />
 
-            </div>
-         <div class="row">
-            <div  class="tab-content">
-              <div
-                class="tab-pane "
-                id="layout"
-                role="tabpanel">
-                        <div class="row">
-
-                      <section>
-                          <div >
-                          <h2>Selected</h2>
-
-                          <form class="choice" action="{{ url('add-mod-fields') }}" method="POST">
-                            @csrf
-                            <label>Form</label>
-                          
-                            
-                            <button   type="submit" class="btn btn-primary motom" >Submit</button>
-                             <input type="hidden"  name="module[]" value="{{$data['selected_fields'][0]->module_id}}">
-                           @foreach($data['selected_fields'] as $filelist )
-
-                                <li draggable="true">
-
-                                  <label> {{ str_replace("_"," ",strtoupper( $filelist->col_name )) }}</label>
-                                    <input type="hidden"  name="module" value="{{$filelist->module_id}}">
-                                     <input type="hidden"   name="module_id[]" value="{{$filelist->module_id}}">  
-                                     <input type="hidden" name="column_id[]" value="{{$filelist->column_id}}">
-                                     <input type="hidden"   name="type[]" value="{{$filelist->type}}" >
-                                      <input type="hidden"  name="col_name[]" value="{{$filelist->col_name}}"  > 
-
-                               </li>
-
-                        @endforeach
-                          </form>
-                               
-                          </div>
-
-                          <hr>
-                            
-                         <div class="choice">
-                            <h2>Unselected</h2>
-                          @foreach( $data['all_fields'] as $val) 
-                               @if(!in_array($val->column_id,explode(",", $selfield1))) 
-                               <li draggable="true">
-                                   <label>{{ str_replace("_"," ",strtoupper($val->col_name)) }}</label>
-                                     <input type="hidden"  name="module" value="{{$data['mid']}}">
-                                    <input type="hidden"   name="column_id[]" value=" {{$val->column_id }}" > 
-                                      <input type="hidden"   name="type[]" value=" {{$val->type}} " >
-                                     <input type="hidden"   name="col_name[]" value="{{ $val->col_name}} " > 
-
-                                           </li>
-                                      
-                                    @endif
-                                    @endforeach
-                                   
-                                
-                              </div>
-                            
-                    
-
-                             </section>
-             
-                      </div>
-
-              </div>
-
-
-             
-            </div>
-          </div>
-
-
+        <form  action="{{ url('add-mod-fields') }}" method="POST">
+          @csrf
+          <label>Form</label>
+          <input type="hidden"  name="module[]" value="{{$data['selected_fields'][0]->module_id}}">
+          <ul class="list-arrow choice">
+            @foreach($data['selected_fields'] as $filelist )
+            <li draggable="true">
+              <label> {{ str_replace("_"," ",strtoupper( $filelist->col_name )) }}</label>
+              <input type="hidden"  name="module" value="{{$filelist->module_id}}">
+              <input type="hidden"   name="module_id[]" value="{{$filelist->module_id}}">
+              <input type="hidden" name="column_id[]" value="{{$filelist->column_id}}">
+              <input type="hidden"   name="type[]" value="{{$filelist->type}}" >
+              <input type="hidden"  name="col_name[]" value="{{$filelist->col_name}}"  >
+            </li>
+            @endforeach
+          </ul>
+          <button   type="submit" class="btn btn-info " >Submit</button>
+        </form>
       </div>
-   
-     
-    
     </div>
-    <!-- Tabs content --> 
-    
+    <div class="col-md-8">
+      <div class="Selected_red">
+        <h4><i class="fa fa-times" aria-hidden="true"></i> Unselected</h4>
+         <p class="font-weight-normal mb-2 text-muted">Note : Ashish panwar</p>
+         <hr />
+        <ul class=" choice padd_50">
+          @foreach( $data['all_fields'] as $val) 
+          
+          @if(!in_array($val->column_id,explode(",", $selfield1)))
+          <li draggable="true">
+            <label> {{ str_replace("_"," ",strtoupper($val->col_name)) }} </label>
+            <input type="hidden"  name="module" value="{{$data['mid']}}">
+            <input type="hidden"   name="column_id[]" value=" {{$val->column_id }}" >
+            <input type="hidden"   name="type[]" value=" {{$val->type}} " >
+            <input type="hidden"   name="col_name[]" value="{{ $val->col_name}} " >
+          </li>
+          @endif
+          @endforeach
+        </ul>
+        <p> Note:-  </p>
+      </div>
     </div>
- 
-
-<!--  -->
-
-<!--  -->
+  </div>
+  
+  <!--  --> 
+  
 </div>
-</div>
-@include('footer')
-
-
-<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
- 
+@include('footer') 
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script> 
 <script type="text/javascript">
   var input=document.getElementsByTagName('li');
   var choice = document.getElementsByClassName('choice');
@@ -201,5 +160,4 @@ $fieldl[]=$selected_field->column_id;
   $(function  () {
   $("form.choice").sortable();
 });
-</script>
-  
+</script> 
