3ab0ffb0ca54e20d952bcb51991ad3aaea2367b7
ashish panwar <bulk24sms.ashishpanwar@gmail.com>
2022-10-18 13:01:57 +0530

    ashish

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,143 +5,34 @@
   position: absolute;
   padding: 0px;
   margin-top: 10px;
-  border: 2px solid #FF8C00;
+  border: 2px solid #eee;
+  bottom: 80px;
 }
 .show_hide {
     display:block;
 }
+.Reminder_btn a {
+  border: 2px dotted #ccc;
+}
 
 
+.slidingDiv1 {
+  position: absolute;
+  padding: 0px;
+  margin-top: 10px;
+  border: 2px solid #eee;
+  bottom: 80px;
+}
+.show_hide1 {
+    display:block;
+}
 
 
-
-
-
-    .my_div_ashish {
-        width: 350px;
-        height: auto;
-    }
-
-    .my_div_ashish input {
-        opacity: 1;
-    }
-
-    .my_div_ashish input {
-        opacity: 1 !important;
-        width: 100% !important;
-        margin: 5px !important;
-        padding: 18px 10px !important;
-
-    }
-
-    .switch {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 34px;
-    }
-
-    .switch input {
-        opacity: 0;
-        width: 0;
-        height: 0;
-    }
-
-    .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ccc;
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
-
-    .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
-
-    input:checked+.slider {
-        background-color: #2196F3;
-    }
-
-    input:focus+.slider {
-        box-shadow: 0 0 1px #2196F3;
-    }
-
-    input:checked+.slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
-    }
-
-    /* Rounded sliders */
-    .slider.round {
-        border-radius: 34px;
-    }
-
-    .slider.round:before {
-        border-radius: 50%;
-    }
-
-    .dropbtn {
-        background-color: #3498DB;
-        color: white;
-        padding: 16px;
-        font-size: 16px;
-        border: none;
-        cursor: pointer;
-    }
-
-    .dropbtn:hover,
-    .dropbtn:focus {
-        background-color: #2980B9;
-    }
-
-    .dropdown {
-        position: relative;
-        display: inline-block;
-    }
-
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f1f1f1;
-        min-width: 160px;
-        overflow: auto;
-        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-        z-index: 1;
-    }
-
-    .dropdown-content a {
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-    }
-
-    .dropdown a:hover {
-        background-color: #ddd;
-    }
-
-    .show {
-        display: block;
-    }
 </style>
 <div class="content-wrapper">
   <div class="row">
     <div class="col-sm-12 mb-4 mb-xl-0">
-      <h4 class="font-weight-bold text-dark">Add Tasks </h4>
+      <h4 class="font-weight-bold text-dark"><i class="fa fa-file-text-o menu-icon"></i> Add Tasks </h4>
     </div>
   </div>
   <div class="row">
@@ -170,25 +61,13 @@
                   <input class="form-control" name="contect" id="reporting_to">
                 </div>
               </div>
-              <div class=" col-md-6" id="container">
-                <div class="form-group">
-                  <label for="stage_name">Account: </label>
-                  <input class="form-control" name="account" id="reporting_to">
-                </div>
-              </div>
-              <div class=" col-md-6">
-                <div class="form-group">
-                  <label for="usr"> Description <font style="color:red;">*</font></label>
-                  <textarea name="description" placeholder="Description" class="form-control "></textarea>
-                </div>
-              </div>
-              <div class=" col-md-3" id="container">
+              <div class=" col-md-4" id="container">
                 <div class="form-group">
                   <label for="stage_name">DueDate: </label>
                   <input class="form-control" type="date" name="duedate" value="yyyy/mm/dd">
                 </div>
               </div>
-              <div class=" col-md-3" id="container">
+              <div class=" col-md-4" id="container">
                 <div class="form-group">
                   <label for="stage_name">Status: </label>
                   <select class="form-control" id="" name="status">
@@ -198,7 +77,7 @@
                   </select>
                 </div>
               </div>
-              <div class=" col-md-3" id="container">
+              <div class=" col-md-4" id="container">
                 <div class="form-group">
                   <label for="">Priority: </label>
                   <select class="form-control" id="" name="priority">
@@ -209,68 +88,125 @@
                   </select>
                 </div>
               </div>
-              {{--
-              <div class=" col-md-3" id="container">
+              <div class=" col-md-12">
                 <div class="form-group">
-                  <label for="stage_name">Repeat: </label>
-                  <label class="switch">
-                  <input id="reminder1" type="checkbox">
-                  <span class="slider round"></span>
-                  <div id="myDropdown" class="dropdown-content"> <a href="#home">Home</a> <a href="#about">About</a> <a href="#contact">Contact</a> </div>
-                  </label>
+                  <label for="usr"> Description <font style="color:red;">*</font></label>
+                  <textarea name="description" placeholder="Description" class="form-control "></textarea>
                 </div>
               </div>
-              --}}
-              <div class=" col-md-3" id="container">
-               
-              <a href="#" class="show_hide btn btn-info"> Reminder </a>
-              
+              <div class=" col-md-4" id="container">
+                <div class="form-group">
+                  <label for="stage_name">Account: </label>
+                  <input class="form-control" name="account" id="reporting_to">
+                </div>
+              </div>
+              <div class=" col-md-4" id="container">
+                <label for="stage_name"> Add Reminder </label>
+                <div class="Reminder_btn"> <a href="#" class="show_hide btn "> <i class="fa fa-bell" aria-hidden="true"></i> Reminder </a> </div>
                 <div class="slidingDiv" style="display: block;">
-                <div class="card">
-                <div class="card-body">
-                <div class="row">
-                
-                <div class="col-md-12">
-               <div class="form-group">
-               
-                <label for=""> Select Date & Time </label>
-                    <input class="form-control" type="date" value="yyyy/mm/dd/hh/mm" name="taskowner"
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label for=""> Select Date & Time </label>
+                            <input class="form-control" type="date" value="yyyy/mm/dd hh/mm" name="taskowner"
                                                     placeholder="Name" required="">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label for="">Notify User </label>
+                            <select class="form-control" id="" name="priority">
+                              <option value="">Notify</option>
+                              <option value="high">Popup</option>
+                              <option value="lowest">Email</option>
+                              <option value="normal">Both</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                
-                <div class="col-md-12">
-                <div class="form-group">
-                  <label for="">Notify User </label>
-                  <select class="form-control" id="" name="priority">
-                    <option value="">Notify</option>
-                    <option value="high">Popup</option>
-                    <option value="lowest">Email</option>
-                    <option value="normal">Both</option>
-                  </select>
+              </div>
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              <div class=" col-md-4" id="container">
+                <label for="stage_name">Add Repeat </label>
+                <div class="Reminder_btn"> <a href="#" class="show_hide1 btn "> Repeat </a> </div>
+                <div class="slidingDiv1" style="display: block;">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label for=""> Repeat </label>
+                            <select class="form-control" id="" name="priority">
+                              <option value="">Daily</option>
+                              <option value="high">Weekly</option>
+                              <option value="lowest">Monthly</option>
+                              <option value="normal">Yearly</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-check">
+                            <label class="form-check-label">
+                              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="">
+                              Never <i class="input-helper"></i></label>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="row">
+                            <div class="col-md-3">
+                              <div class="form-check">
+                                <label class="form-check-label">
+                                  <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="">
+                                  After <i class="input-helper"></i></label>
+                              </div>
+                            </div>
+                            <div class="col-md-9">
+                              <div class="form-group row">
+                                <div class="col-md-6"> <input type="number"  class="form-control" placeholder=""> </div>
+                                <div class="col-md-6">  <label class="mt-2" for=""> Times </label> </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="row">
+                            <div class="col-md-3">
+                              <div class="form-check">
+                                <label class="form-check-label">
+                                  <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="">
+                                  On <i class="input-helper"></i></label>
+                              </div>
+                            </div>
+                            <div class="col-md-9">
+                              <div class="form-group">
+                                <input class="form-control" type="date" value="yyyy/mm/dd hh/mm" name="taskowner"
+                                                    placeholder="Name" required="">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                </div>
-                
-                
-                
-                </div>
-                
-                </div>
-                </div>
-                </div>
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
               </div>
             </div>
-            <div class=" col-md-4 mt-4">
+            <div class=" col-md-12 text-right mt-4">
               <button type="submit" class="btn btn-info font-weight-bold" id=""> Submit </button>
             </div>
           </form>
@@ -282,11 +218,8 @@
 @include('footer') 
 <script>
 $(document).ready(function(){
-
-
 $(".slidingDiv").hide();
 	$(".show_hide").show();
-	
 	$('.show_hide').click(function(){
 	$(".slidingDiv").slideToggle();
 	});
@@ -294,3 +227,15 @@ $(".slidingDiv").hide();
 });
 
 </script> 
+<script>
+
+
+$(document).ready(function(){
+$(".slidingDiv1").hide();
+	$(".show_hide1").show();
+	$('.show_hide1').click(function(){
+	$(".slidingDiv1").slideToggle();
+	});
+
+});
+</script>
